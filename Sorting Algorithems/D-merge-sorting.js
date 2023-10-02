33949ae48f4ffa4745e410b5e4ebb0708a18fd1a
shivamkumarjha711 <shivamkumarjha711@gmail.com>
2023-10-02 10:16:46 +0530

    Sorting algorithem done

---
+++ Sorting Algorithems/D-merge-sorting.js
@@ -0,0 +1,25 @@
+function mergeSort(arr) {
+    if (arr.length < 2) {
+        return arr
+    }
+    const mid = Math.floor(arr.length / 2)
+    const leftArr = arr.slice(0, mid)
+    const rightArr = arr.slice(mid)
+    return merge(mergeSort(leftArr), mergeSort(rightArr))
+}
+
+function merge(leftArr, rightArr) {
+    const sortArr = []
+    while (leftArr.length && rightArr.length) {
+        if (leftArr[0] <= rightArr[0]) {
+            sortArr.push(leftArr.shift())
+        } else {
+            sortArr.push(rightArr.shift())
+        }
+    }
+    return [...sortArr, ...leftArr, ...rightArr]
+}
+
+const arr = [8, 20, -2, 4, -6]
+console.log(mergeSort(arr));  // -6, -2, 4, 8, 20  m
+

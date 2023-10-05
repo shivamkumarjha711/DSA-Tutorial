29688351a22dd130d72b80c41b0a9450104c3c25
shivamkumarjha711 <shivamkumarjha711@gmail.com>
2023-10-05 21:03:43 +0530

    stairecas solution done

---
+++ Mislaneous Questions/B-Climbing-Staircase.js
@@ -0,0 +1,14 @@
+function climbingStaircase(n) {
+    const noOfways = [1, 2]
+    for(let i = 2; i <= n; i++) {
+        noOfways[i] = noOfways[i-1] + noOfways[i-2]
+    }
+    return noOfways[n-1]
+}
+
+
+console.log(climbingStaircase(1));
+console.log(climbingStaircase(2));
+console.log(climbingStaircase(3));
+console.log(climbingStaircase(4));
+console.log(climbingStaircase(5));

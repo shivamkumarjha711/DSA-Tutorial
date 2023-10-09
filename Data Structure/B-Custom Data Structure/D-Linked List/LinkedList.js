f0cee89adfe3cd897effa087825c2e6e44bd8ee4
shivamkumarjha711 <shivamkumarjha711@gmail.com>
2023-10-09 23:34:26 +0530

    Linked List remove by index done

--- Data Structure/B-Custom Data Structure/D-Linked List/LinkedList.js
+++ Data Structure/B-Custom Data Structure/D-Linked List/LinkedList.js
@@ -19,7 +19,8 @@ class LinkedList {
         return this.size
     }
 
-    prepend(value) {          // Adding new "Node"
+    // Big - O(1)    (bcz "Node" add in satring)
+    prepend(value) {          // Adding new "Node" from the begining
         const node = new Node(value)   // Creating new Instance of Node Class
         if (this.isEmpty()) {
             this.head = node
@@ -28,6 +29,60 @@ class LinkedList {
             this.head = node
         }
         this.size++
+    }
+
+    // Big - 0(n)    (bcz "Node" add at end)
+    append(value) {               // Adding new "Node" at the end
+        const node = new Node(value)
+        if (this.isEmpty()) {
+            this.head = node
+        } else {
+            let prev = this.head
+            while(prev.next) {
+                prev = prev.next
+            }
+            prev.next = node
+        }
+        this.size++
+    }
+
+    insert(value, index) {            // Adding new "Node" at the given Index value
+        if (index < 0 || index > this.size) {
+            return
+        }
+        if (index === 0) {
+            this.prepend(value)
+        } else {
+            const node = new Node(value)
+            let prev = this.head
+            for (let i = 0; i < index-1; i++) {
+                prev = prev.next
+            }
+            node.next = prev.next
+            prev.next = node
+            this.size++
+        }
+    }
+
+    removeFrom(index) {
+        if (index < 0 || index >= this.size) {
+            console.log("Index is not found");
+            return null
+        }
+        let removeNode
+        if (index === 0) {
+            removeNode = this.head
+            this.head = this.head.next
+        } else {
+            let prev = this.head
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next
+            }
+            removeNode = prev.next
+            prev.next = removeNode.value
+        }
+        this.size--
+        return removeNode.value
     }
 
     print() {
@@ -50,9 +105,41 @@ console.log('List is Empty ?', list.isEmpty());
 console.log('List Size', list.getSize());
 list.print()
 
-list.prepend(10)
+// list.prepend(10)
+// list.print()
+
+// list.append(10)
+// list.print()
+
+// list.prepend(20)
+// list.prepend(30)
+// list.print()
+
+// list.append(20)
+// list.append(30)
+// list.print()
+
+list.insert(10, 0)
 list.print()
 
-list.prepend(20)
-list.prepend(30)
+list.insert(20, 0)
 list.print()
+
+list.insert(30, 1)
+list.print()
+
+list.insert(40, 2)
+list.print()
+console.log(list.getSize());
+
+console.log(list.removeFrom(10));
+
+console.log(list.removeFrom(0));
+list.print()
+
+console.log(list.removeFrom(1));
+list.print()
+console.log(list.getSize());
+
+
+
